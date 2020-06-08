73464189ea9c6de6dd1aadfb078d86ab8cb6245b
jsymondst <jammiesymondstayler@gmail.com>
2020-06-08 14:50:48 +0100

    Done.

--- rendering/app/controllers/responses_controller.rb
+++ rendering/app/controllers/responses_controller.rb
@@ -5,6 +5,7 @@ class ResponsesController < ApplicationController
   # render the data according to the name of the action
   def plain
     data = "Early bird gets the worm. But cookie taste better than worm. So me sleep in. - Cookie Monster"
+    render plain: data
   end
 
   def html
@@ -14,6 +15,8 @@ class ResponsesController < ApplicationController
                 <cite>Cookie Monster</cite>
               </body>
             </html>".html_safe
+      render html: data
+
   end
 
   def json
@@ -21,6 +24,7 @@ class ResponsesController < ApplicationController
       author: "Cookie Monster",
       quote:"Early bird gets the worm. But cookie taste better than worm. So me sleep in.",
     }
+    render json: data
   end
 
   def xml
@@ -28,6 +32,7 @@ class ResponsesController < ApplicationController
     <author>Cookie Monster</author>
     <text>Early bird gets the worm. But cookie taste better than worm. So me sleep in.</text>
     </quote>"
+    render xml: data
   end
 
   def csv
@@ -35,6 +40,7 @@ class ResponsesController < ApplicationController
       csv << ["author", "name"]
       csv << ["Cookie Monster", "Early bird gets the worm. But cookie taste better than worm. So me sleep in."]
     end
+    send_data csv: data
 
     # you'll need to use send_data instead of render
     # if you want the file to have a particular filename, pass the filename: parameter
